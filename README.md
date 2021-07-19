06d2a48923882bdf2036e936af53f5731e8fa614
Luke Abraham <luke.abraham@gmail.com>
2021-07-19 16:28:51 +0100

    updated scripts to use Iris 3.0.3 and Python 3.8

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # ukca
-Scripts for working with the UKCA Composition-Climate Model: www.ukca.ac.uk
+Scripts for working with the [UKCA Composition-Climate Model](https://www.ukca.ac.uk/)
 
-**Note**: If using `conda` to install Iris, these scripts work best with the following options
+**Note**: If using `conda` to install Iris, the scripts in the `vn10.9` and `vn11.8` directories work best with the following options
 
     conda install -y -c conda-forge python=3.6 iris=1.13 ipython mo_pack numpy=1.15
 
 if using [Miniconda3](https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh).
 
 They will have errors if using Iris 2.4.0 unless version 1.2.1 of `cftime` is installed (they will fail with version 1.3.0).
+
+The scripts in the `vn11.8_iris3` directory work when installing Iris 3+ using
+
+    conda install -y -c conda-forge iris ipython mo_pack
+    
+Using [mamba](https://github.com/mamba-org/mamba) may give better performance when installing Iris and its dependencies.
---
+++ Tutorials/vn11.8_iris3/Task10/Task104_write_TOA.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+# This file is part of the UKCA Tutorials:
+#  http://www.ukca.ac.uk/wiki/index.php/UKCA_Chemistry_and_Aerosol_Tutorials_at_vn11.8
+
+# Copyright (C) 2021  University of Cambridge
+
+# This is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+
+# It is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+
+# You find a copy of the GNU Lesser General Public License at <http://www.gnu.org/licenses/>.
+
+# Written by N. Luke Abraham 2021-01-18 <nla27@cam.ac.uk> 
+
+# preamble
+import iris
+import iris.time
+
+fname='/home/ubuntu/Tutorials/vn11.8/sample_output/Task10.1/atmosa.pa19810901_00'
+
+# constraint on time to get 2nd radiation timestep
+tconstr=iris.Constraint(time=lambda cell: cell.point.hour == 2)
+
+# load all TOA components at 0.55 micron
+isw=iris.load_cube(fname,[iris.AttributeConstraint(STASH='m01s01i207') & tconstr])
+osw=iris.load_cube(fname,[iris.AttributeConstraint(STASH='m01s01i208') & tconstr])
+olw=iris.load_cube(fname,[iris.AttributeConstraint(STASH='m01s02i205') & tconstr])
+
+# make cube to store net downward TOA flux
+toa=isw.copy()
+# add-up components
+toa.data=isw.data - (osw.data + olw.data)
+
+toa.rename('toa_net_downward_radiative_flux')
+
+# output to netCDF
+iris.save(toa,'Task104_TOA.nc',netcdf_format='NETCDF3_CLASSIC')
